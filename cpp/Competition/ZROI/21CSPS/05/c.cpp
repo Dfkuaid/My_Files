3c57de32be150fcaf73b733ec02767b51dba0deb
dfkuaid <2106660898@qq.com>
2021-09-26 10:13:00 +0800

    ZROI-21-CSP-DAY5_C

--- cpp/Competition/ZROI/21CSPS/05/c.cpp
+++ cpp/Competition/ZROI/21CSPS/05/c.cpp
@@ -3,33 +3,29 @@
 using namespace std;
 
 const int N = 200010;
+const int INF = 0x3fffffff;
 
 struct Edge {int u, v, nxt;} e[N], ae[N];
 
-int n, m, ecnt(1), head[N], icnt[N], q[N], frt, tal, mx[N][3];
+int ecnt(1), icnt[N], head[N];
 
 inline void add_edge(int u, int v) {
     e[ecnt].u = u, e[ecnt].v = v, ++ icnt[v];
     e[ecnt].nxt = head[u], head[u] = ecnt ++;
 }
 
-inline int GetType(int x) {
-    // printf("%d %d %d %d\n", x, mx[x][1], mx[x][2], mx[x][3]);
-    if (mx[x][1] <= mx[x][2] && mx[x][1] <= mx[x][3]) return 1;
-    else if (mx[x][2] <= mx[x][1] && mx[x][2] <= mx[x][3]) return 2;
-    else return 3;
-}
+int n, m, q[N], frt, tal, T1, T2, T3, type[N][3];
 
-void solve() {
+void topo() {
     frt = 0, tal = -1;
     for (int i = 1; i <= n; ++ i)
       if (!icnt[i]) q[++ tal] = i;
     while (frt <= tal) {
-        int now = q[frt ++], op = GetType(now);
-        for (int i = head[now]; i; i = e[i].nxt) {
-            mx[e[i].v][op] = max(mx[e[i].v][op], mx[now][op] + 1);
-            -- icnt[e[i].v]; if (!icnt[e[i].v]) q[++ tal] = e[i].v;
-        }
+        int now = q[frt ++]; (++ T1) %= 42;
+        if (T1 % 42 == 1) ++ T2; if (T2 % 42 == 1) ++ T3;
+        type[now][2] = T3, type[now][1] = T2;
+        for (int i = head[now]; i; i = e[i].nxt)
+          if (!(-- icnt[e[i].v])) q[++ tal] = e[i].v;
     }
 }
 
@@ -40,16 +36,14 @@ int main() {
 #endif
     scanf("%d%d", &n, &m);
     for (int i = 1; i <= m; ++ i) {
-        int u, v; scanf("%d%d", &u, &v);
-        // printf("%d %d\n", u, v);
-        add_edge(u, v); ae[i].u = u, ae[i].v = v;
+        scanf("%d%d", &ae[i].u, &ae[i].v);
+        add_edge(ae[i].u, ae[i].v);
     }
-    solve();
+    topo();
     for (int i = 1; i <= m; ++ i) {
-        // printf("%d %d", ae[i].u, ae[i].v);
-        int op = GetType(ae[i].u);
-        if (op == 1) printf("R\n");
-        else if (op == 2) printf("G\n");
+        int u = ae[i].u, v = ae[i].v;
+        if (type[u][1] == type[v][1]) printf("R\n");
+        else if (type[u][2] == type[v][2]) printf("G\n");
         else printf("B\n");
     }
     return 0;
--- cpp/Competition/ZROI/21CSPS/05/d.cpp
+++ cpp/Competition/ZROI/21CSPS/05/d.cpp
@@ -1,62 +1,8 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
 const int N = 1010;
 
-struct Point {
-    int x, y;
-
-    inline Point operator - (const Point b) const {
-        return (Point){x - b.x, y - b.y};
-    }
-
-    inline Point operator + (const Point b) const {
-        return (Point){x + b.x, y + b.y};
-    }
-} p[N];
-
-inline ll dot(Point a, Point b) {return a.x * b.x + a.y * b.y;}
-inline ll cross(Point a, Point b) {return 1ll * a.x * b.y - 1ll * a.y * b.x;}
-inline ll dis(Point a, Point b) {return dot(a - b, a - b);}
-
-
-bool intersection(Point a, Point b, Point c, Point d) {
-    // printf("%d,%d %d,%d %d,%d %d,%d\n", a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
-    // printf(">> %d %d\n", max(a.x, b.x), min(c.x, d.x));
-    if (max(c.x, d.x) <= min(a.x, b.x) || max(a.x, b.x) <= min(c.x, d.x) || max(c.y, d.y) <= min(a.y, b.y) || max(a.y, b.y) <= min(c.y, d.y)) return false;
-    // printf("test one unpass.\n");
-    ll res1 = cross(a - d, c - d), res2 = cross(b - d, c - d);
-    ll res3 = cross(d - b, a - b), res4 = cross(c - b, a - b);
-    if ((res1 <= 0 && res2 <= 0) || (res1 >= 0 && res2 >= 0)) return false;
-    if ((res3 <= 0 && res4 <= 0) || (res3 >= 0 && res4 >= 0)) return false;
-    return true;
-}
-
-
-int n, ans;
-
-bool check(int a, int b) {
-    // printf("now: %d %d\n", a, b);
-    for (int i = 1; i <= n; ++ i)
-      for (int j = i + 1; j <= n; ++ j) {
-          if ((a == i && b == j) || (a == j && b == i)) continue;
-          // printf("Test: %d %d\n", i, j);
-          if (intersection(p[a], p[b], p[i], p[j])) return false;
-      }
-    return true;
-}
-
 int main() {
-    scanf("%d", &n);
-    for (int i = 1; i <= n; ++ i)
-      scanf("%d%d", &p[i].x, &p[i].y);
-    for (int i = 1; i <= n; ++ i)
-      for (int j = i + 1; j <= n; ++ j)
-        if (check(i, j)) {
-            // printf("%d %d\n", i, j);
-            ++ ans;
-        } 
-    printf("%d", ans);
     return 0;
 }
